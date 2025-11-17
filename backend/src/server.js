003b4e21df1e20e378526fff55a49ebd94f785a5
ashmitrana24 <ashmit.rana2031@gmail.com>
2025-11-18 02:16:12 +0530

    server side auth added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,11 @@ import { ENV } from './lib/env.js';
 import path from 'path';
 import cors from 'cors';
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './lib/db.js';
 import { inngest , functions } from './lib/inngest.js';
+import { protectRoute } from './middleware/protectRoute.js';
+import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 
@@ -14,16 +17,16 @@ const __dirname = path.resolve();
 app.use(express.json());
 //credentials : true means server allows to include cookies on requests
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("api/chat", chatRoutes)
 
 app.get('/health', (req, res) => {
-  res.status(200).json({ message: 'Server is running' });
+  res.status(200).json({ message: 'Connected to API' });
 });
 
-app.get('/books', (req, res) => {
-  res.status(200).json({ message: 'API is running' });
-});
+
 
 
 if(ENV.NODE_ENV === 'production'){

5ef11379a80f2f33c27aeb7569991506de89bc30
ashmitrana24 <ashmit.rana2031@gmail.com>
2025-11-18 09:51:55 +0530

    session routes and controllers added

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,14 +1,16 @@
 import { StreamChat } from "stream-chat";
+import { StreamClient } from "@stream-io/node-sdk";
 import { ENV } from "./env.js";
 
 const apiKey = ENV.STREAM_API_KEY;
 const apiSecret = ENV.STREAM_API_SECRET;
 
-if(!apiKey || !apiSecret) {
-  throw new Error("Stream API key and secret must be provided");
+if (!apiKey || !apiSecret) {
+  console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
 }
 
-export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
+export const streamClient = new StreamClient(apiKey, apiSecret); // will be used for video calls
 
 export const upsertStreamUser = async (userData) => {
   try {
--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { connectDB } from './lib/db.js';
 import { inngest , functions } from './lib/inngest.js';
 import { protectRoute } from './middleware/protectRoute.js';
 import chatRoutes from './routes/chatRoutes.js';
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
@@ -20,7 +21,8 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("api/chat", chatRoutes)
+app.use("/api/chat", chatRoutes)
+app.use("/api/sessions", sessionRoutes);
 
 app.get('/health', (req, res) => {
   res.status(200).json({ message: 'Connected to API' });
